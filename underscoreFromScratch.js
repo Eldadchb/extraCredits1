aa89d1b86978e833ec1e1a3a21e4bc3731278ece
Eldad Chidra <eldadchb@gmail.com>
2022-11-08 17:24:16 +0100

    add: initial

--- underscoreFromScratch.js
+++ underscoreFromScratch.js
@@ -14,3 +14,16 @@ function find(list, predicate){
 // var even = find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
 // console.log(even)
 
+function initial(array){
+    let args = [...arguments];
+
+    if (args.length > 1){
+        return array.slice(0, arguments[1]);
+
+    } else {
+        return array.slice(0, array[array.length - 2])
+    }
+}
+
+// let test = initial([1,2,3,4,5], 2);
+// console.log(test);
