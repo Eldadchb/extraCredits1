c9e4fef6123ceb958767b51a38b6bd018674a2be
Eldad Chidra <eldadchb@gmail.com>
2022-11-08 18:16:07 +0100

    add: rest and binarySearch

--- underscoreFromScratch.js
+++ underscoreFromScratch.js
@@ -27,3 +27,66 @@ function initial(array){
 
 // let test = initial([1,2,3,4,5], 2);
 // console.log(test);
+
+function rest(array){
+    let args = [...arguments];
+
+    if (args.length > 1){
+        return array.slice(arguments[1], array[array.length - 1]);
+
+    } else {
+        return array.slice(1, array[array.length - 1])
+    }
+}
+
+// let test = rest([1,2,3,4,5], 2);
+// console.log(test);
+
+function indexOf(array, value){
+    let args = [...arguments];
+    let isSorted = false;
+    let result;
+
+    if(args.length > 2){
+        if(args[2]){
+            isSorted = true;
+        }
+    }
+    if(isSorted){
+        result = binarySearch(array, value, 0, array.length - 1)
+        if(result){
+            return result
+        } else {
+            return -1;
+        }
+
+    }else{
+        for(let i = 0 ; i < array.length ; i++){
+            if(array[i] === value){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+function binarySearch(array, reqVal, start, end){
+    if(start > end) return false;
+
+    let mid = Math.floor((start + end)/2);
+
+    if(array[mid] === reqVal) return mid;
+
+    if(array[mid] > reqVal){
+
+        return binarySearch(array, reqVal, start, mid-1);
+
+    } else {
+
+        return binarySearch(array, reqVal, mid+1, end);
+
+    }
+}
+
+// let test = indexOf([1,2,3,4,5,6,7,8,11,35,67,88,9000,10000], 35, true);
+// console.log(test);
